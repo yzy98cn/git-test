e98d32f29018de7a0a5d97aaa123fbcf32f33b96
yzywithwdd <1712561158@qq.com>
2022-09-30 09:31:31 +0800

    第一次提交

---
+++ src/main/java/com/git/App.java
@@ -0,0 +1,13 @@
+package com.git;
+
+/**
+ * @author  {yzy}
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
