a55d4702c6aed4d0f96a7456df5d4d679f0106c1
yzywithwdd <1712561158@qq.com>
2022-09-30 09:52:04 +0800

    hot-fix修改了

--- src/main/java/com/git/App.java
+++ src/main/java/com/git/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World3" );
         System.out.println( "Hello World3" );
         System.out.println( "Hello World3" );
+        System.out.println( "hot-fix 修改了" );
     }
 }
